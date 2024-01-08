40b7fb3a6e172cee76d328302d9951b844bb8056
teahan <thanhnd@mogroup.com>
2024-01-09 00:00:27 +0700

    add config

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -3,5 +3,5 @@ export const LinkedInApi = {
     client_id: "86r7fz2emz2vp6",
     redirect_uri: "https://hanstack-linkedin-tool.vercel.app",
     state: "DCEeFWf45A53sdfKef424",
-    scope: "liteprofile%20emailaddress%20w_member_social"
+    scope: "w_member_social"
 }
