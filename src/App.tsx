0d98cc5966628fe7c4b397f40e81dd7db9e71505
teahan <thanhnd@mogroup.com>
2024-01-08 23:16:20 +0700

    Add linkendin oauth2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import axios from "axios"
 
 function App() {
-
+  function authorizeLinkedin() {
+    axios.get('https://www.linkedin.com/oauth/v2/authorization', {
+      params: {
+        response_type: "code",
+        client_id: "86r7fz2emz2vp6",
+        redirect_uri: "https://hanstack-linkedin-tool.vercel.app",
+        state: "DCEeFWf45A53sdfKef424",
+        scope: "liteprofile%20emailaddress%20w_member_social"
+      }
+    }).then(function (response) {
+      console.log(response);
+    })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        console.log("finally");
+      });
+  }
   return (
     <>
       <div>
-        
+        <button onClick={() => authorizeLinkedin()}>Connect to Linken</button>
       </div>
     </>
   )
